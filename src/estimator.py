29c487f8eee62e0587d93b88097d5a6e0a86c4e1
MuhammadZulhaydar <haydaromarjelambar@gmail.com>
2026-05-24 20:42:46 +0700

    menyelesaikan job B .py & .ipynb

--- src/estimator.py
+++ src/estimator.py
@@ -28,9 +28,73 @@ def beta_posterior_params(k, m):
     beta_post = m + 1
     return alpha_post, beta_post
 
-#TODO: Still not yet implementing the beta mean and mode
-def beta_mean():
-    raise NotImplementedError
+def beta_mode(alpha, beta):
+    """
+    Menghitung mode dari distribusi Beta.
+    
+    Rumus:
+    mode = (alpha - 1) / (alpha + beta - 2)
 
-def beta_mode():
-    raise NotImplementedError
+    Syarat:
+    alpha > 1 dan beta > 1
+    """
+    if alpha <= 1 or beta <= 1:
+        return "Mode tidak terdefinisi untuk alpha <= 1 atau beta <= 1"
+    
+    return (alpha - 1) / (alpha + beta - 2)
+
+
+def beta_mean(alpha, beta):
+    """
+    Menghitung mean/rata-rata dari distribusi Beta.
+    
+    Rumus:
+    mean = alpha / (alpha + beta)
+    """
+    if (alpha + beta) == 0:
+        return 0
+    
+    return alpha / (alpha + beta)
+
+def log_likelihood_bernoulli(theta, k, n):
+    """
+    Menghitung log-likelihood distribusi Bernoulli.
+
+    theta = probabilitas sukses
+    k     = jumlah sukses
+    n     = total percobaan
+
+    Rumus:
+    log L(theta) = k*log(theta) + (n-k)*log(1-theta)
+    """
+
+    if theta <= 0 or theta >= 1:
+        return float('-inf')
+
+    return (k * math.log(theta)) + ((n - k) * math.log(1 - theta))
+
+
+def log_likelihood_poisson(theta, data):
+    """
+    Menghitung log-likelihood distribusi Poisson.
+
+    theta = parameter lambda Poisson
+    data  = list/array data diskrit
+
+    Rumus:
+    log L(theta) = Σ [x_i * log(theta) - theta - log(x_i!)]
+    """
+
+    if theta <= 0:
+        return float('-inf')
+
+    log_likelihood = 0
+
+    for x in data:
+        log_likelihood += (
+            x * math.log(theta)
+            - theta
+            - math.log(math.factorial(x))
+        )
+
+    return log_likelihood
